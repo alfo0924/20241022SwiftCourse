c2be4031ad09e816c1e282a66fae16eabeb86e89
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-10-22 11:16:16 +0800

    Update Button.swift

--- Button.swift
+++ Button.swift
@@ -9,21 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button(action: {
-            print("Hello World tapped!")
-        }) {
-            Text("Hello 薛佬")
-                .fontWeight(.bold)
-                .font(.title)
+        VStack(spacing: 20) {
+            Button(action: {
+                print("Hello World tapped!")
+            }) {
+                Text("Hello 薛佬")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .padding()
+                    .background(Color.purple)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color.purple, lineWidth: 5)
+                    )
+            }
+            
+            Button(action: {
+                print("Delete tapped!")
+            }) {
+                HStack {
+                    Image(systemName: "trash")
+                        .font(.title)
+                    Text("Delete")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }
                 .padding()
-                .background(Color.purple)
-                .cornerRadius(40)
                 .foregroundColor(.white)
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 40)
-                        .stroke(Color.purple, lineWidth: 5)
-                )
+                .background(Color.red)
+                .cornerRadius(40)
+            }
         }
     }
 }
