eb10cf5ed71c46cbc9e1cc6a65f6ff51d331df2a
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-10-22 11:41:39 +0800

    Update StateAndBindBTN.swift

--- StateAndBindBTN.swift
+++ StateAndBindBTN.swift
@@ -1,66 +1,24 @@
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
     @State private var isPlaying = false
     
     var body: some View {
-        NavigationSplitView {
-            VStack {
-                Button(action: {
-                    // Switch between the play and stop button
-                    self.isPlaying.toggle()
-                }) {
-                    Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
-                        .font(.system(size: 150))
-                        .foregroundColor(isPlaying ? .red : .green)
-                }
-                
-                List {
-                    ForEach(items) { item in
-                        NavigationLink {
-                            Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                        } label: {
-                            Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                        }
-                    }
-                    .onDelete(perform: deleteItems)
-                }
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        Button(action: {
+            // Switch between the play and stop button
+            self.isPlaying.toggle()
+        }) {
+            Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+                .font(.system(size: 150))
+                .foregroundColor(isPlaying ? .red : .green)
         }
-    }
-    
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
     }
 }
 
-#Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
